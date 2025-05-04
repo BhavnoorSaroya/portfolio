b7874f5dbf272350b8dc569d0d8b6c8ba10f4bd9
Bhavnoor Singh Saroya <bsaroya3@my.bcit.ca>
2025-05-03 18:22:11 -0700

    added install script and binary for shakti iso software

---
+++ static/shakti/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo curl -L https://saroya.dev/shakti/shakti -o /usr/local/bin/shakti
+sudo chmod +x /usr/local/bin/shakti
+echo "📦 Installing dependencies..."
+echo "✅ shakti installed! Run with: shakti"
+# One liner: sudo bash <(curl -fsSL https://saroya.dev/shakti/install.sh)
